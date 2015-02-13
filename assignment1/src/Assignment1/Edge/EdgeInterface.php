0318721e229b587bbf002a635e5bc576aa5d6a81
Alan Ly <me@alanly.ca>
2015-02-13 15:26:27 -0500

    Defined the interface for an Edge.

--- assignment1/src/Assignment1/Edge/EdgeInterface.php
+++ assignment1/src/Assignment1/Edge/EdgeInterface.php
@@ -1,4 +1,24 @@
 <?php
 namespace Assignment1\Edge;
 
-interface EdgeInterface {}
+interface EdgeInterface
+{
+	/**
+	 * Returns a collection of the vertices that make up the edge.
+	 * @return \Traversable
+	 */
+	public function vertices();
+
+	/**
+	 * Returns the object that is stored on the edge.
+	 * @return mixed
+	 */
+	public function element();
+
+	/**
+	 * Replace the element at the edge with element $e. Returns the updated edge.
+	 * @param  mixed $e
+	 * @return EdgeInterface
+	 */
+	public function replaceElement($e);
+}
