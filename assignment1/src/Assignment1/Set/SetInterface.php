3eb8036cf1ae1f3ab6e9b022775d6eb6b0bc021e
Alan Ly <me@alanly.ca>
2015-02-13 12:45:35 -0500

    Specified the interface for a Set.

--- assignment1/src/Assignment1/Set/SetInterface.php
+++ assignment1/src/Assignment1/Set/SetInterface.php
@@ -1,4 +1,44 @@
 <?php
 namespace Assignment1\Set;
 
-interface SetInterface {}
+interface SetInterface
+{
+	/**
+	 * Create a new and empty set.
+	 * @return SetInterface
+	 */
+	public function newset();
+
+	/**
+	 * Add element $e to the set and return an updated set.
+	 * @param mixed $e
+	 * @return SetInterface
+	 */
+	public function add($e);
+
+	/**
+	 * Remove element $e from the set and return an updated set.
+	 * @param  mixed $e
+	 * @return SetInterface
+	 */
+	public function remove($e);
+
+	/**
+	 * Return the number of elements in the set.
+	 * @return int
+	 */
+	public function size();
+
+	/**
+	 * Test whether the set is empty or not.
+	 * @return boolean
+	 */
+	public function isEmpty();
+
+	/**
+	 * Test whether the set holds element $e.
+	 * @param  mixed  $e
+	 * @return boolean
+	 */
+	public function isMember($e);
+}
