7f9a851102935a239c5c3053c32bb182f5bf8f09
David Siekut <davidsiekut@gmail.com>
2015-02-22 22:59:06 -0500

    Contract test.

---
+++ assignment2/src/test/ContractTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class ContractTest {
+
+	public static void main(String[] args) {
+		Square s = new Square();
+		//s.setSize(0);
+		s.setSize(1);
+	}
+
+}
---
+++ assignment2/src/test/Square.java
@@ -0,0 +1,12 @@
+package test;
+
+import be.ac.ua.ansymo.adbc.annotations.*;
+
+public class Square {
+
+	@requires({"i > 0"})
+	public void setSize(int i) {
+
+	}
+	
+}
