b27330d52d3519e41152e2dc133cb0df761348e2
Alan Ly <me@alanly.ca>
2015-02-23 17:22:51 -0500

    Bring method into superclass.
    - The isBalance() method needs to be in the super class.

--- assignment2/src/assignment2/BalancedBinTree.java
+++ assignment2/src/assignment2/BalancedBinTree.java
@@ -2,28 +2,11 @@ package assignment2;
 
 import be.ac.ua.ansymo.adbc.annotations.invariant;
 
-@invariant("$this.isBalanced() == true")
+@invariant({"$super", "$this.isBalanced() == true"})
 public class BalancedBinTree extends BinTree {
 
 	public BalancedBinTree(long id) {
 		super(id);
-	}
-
-	public boolean isBalanced() {
-		int lh = 0;
-		int rh = 0;
-		
-		if (this.getLeft() != null) {
-			lh = this.getLeft().height();
-		}
-		if (this.getRight() != null) {
-			rh = this.getRight().height();
-		}
-		if (Math.abs(lh - rh) < 2) {
-			return true;
-		}
-		
-		return false;
 	}
 	
 }
