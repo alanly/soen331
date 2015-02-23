8ce37b864cee6ed8bfb96b8b42afe7159839db6e
Alan Ly <me@alanly.ca>
2015-02-23 17:46:37 -0500

    Add contraints from parent class.

--- assignment2/src/assignment2/FullBinTree.java
+++ assignment2/src/assignment2/FullBinTree.java
@@ -2,7 +2,7 @@ package assignment2;
 
 import be.ac.ua.ansymo.adbc.annotations.invariant;
 
-@invariant("$this.isTwoOrNoLeaf() == true")
+@invariant({"$super", "$this.isTwoOrNoLeaf() == true"})
 public class FullBinTree extends BinTree {
 
 	public FullBinTree(long id) {
