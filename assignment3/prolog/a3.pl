ab25deb6003872076e2cfe8dc500d747f006e362
Alan Ly <me@alanly.ca>
2015-03-30 17:48:17 -0400

    Translate EFSM states to Prolog.

---
+++ assignment3/prolog/a3.pl
@@ -0,0 +1,38 @@
+% ------
+% States
+% ------
+
+% Overall System States
+state(boot).
+state(monitoring).
+state(safe_shutdown).
+state(error_diagnosis).
+
+% Boot States
+state(dormant).
+state(init).
+state(idle).
+
+% Monitoring States
+state(monidle).
+state(regulate_environment).
+state(lockdown).
+
+% Error Diagnosis States
+state(error_rcv).
+state(applicable_rescue).
+state(reset_module_data).
+
+% boot.init States
+state(boot_hw).
+state(senchk).
+state(tchk).
+state(psichk).
+state(ready).
+
+% monitoring.lockdown States
+state(prep_vpurge).
+state(alt_temp).
+state(alt_psi).
+state(risk_assess).
+state(safe_status).
